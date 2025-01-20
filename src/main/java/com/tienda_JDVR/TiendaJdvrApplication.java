fa3d038f689b7a585616d73db066bbf0360c5bf0
JDVargasR <josedavargasr@gmail.com>
2025-01-20 17:00:04 -0600

    Primer commit local

---
+++ src/main/java/com/tienda_JDVR/TiendaJdvrApplication.java
@@ -0,0 +1,13 @@
+package com.tienda_JDVR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaJdvrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaJdvrApplication.class, args);
+	}
+
+}
